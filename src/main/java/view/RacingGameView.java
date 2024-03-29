427f4314fca11e5d003d9aff2ec83b62b94591e0
KumKeeHyun <kbzjung359@gmail.com>
2024-03-29 19:11:02 +0900

    2단계 미션 제출합니다. (#48)
    * refact: View 클래스 응집도를 높이기 위해 Message 클래스 제거
    
    * refact: 입력값 확인 및 재처리 로직을 Controller에서 View로 이동
    
    * refact: 입력값 재처리 로직을 람다를 이용해서 함수로 분리

--- src/main/java/view/RacingGameView.java
+++ src/main/java/view/RacingGameView.java
@@ -4,29 +4,64 @@ import racing.Car;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class RacingGameView {
 
-    public static String getCarNames() {
-        System.out.println(Message.INPUT_CAR_NAMES);
-        return getInput();
+    public static final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_TRY_NUMS_MSG = "시도할 회수는 몇회인가요?";
+    public static final String RESULT_MSG = "실행 결과";
+    public static final String DISPLAY_WINNER_FMT = "%s가 최종 우승했습니다.\n";
+
+    public static String mustGetCarNames() {
+        return retryableInput(RacingGameView::getCarNamesInput, RacingGameView::validateCarNames);
     }
 
-    public static int getTryNums() {
-        System.out.println(Message.INPUT_TRY_NUMS);
-        int tryNums = Integer.parseInt(getInput());
-        System.out.println();
-        return tryNums;
+    private static <T> T retryableInput(Supplier<T> supplier, Predicate<T> validator) {
+        T input = supplier.get();
+        while (!validator.test(input)) {
+            input = supplier.get();
+        }
+        return input;
     }
 
-    public static String getInput() {
+    private static boolean validateCarNames(String carNames) {
+        return !carNames.isEmpty();
+    }
+
+    private static String getCarNamesInput() {
+        return getInput(INPUT_CAR_NAMES_MSG);
+    }
+
+    public static int mustGetTryNums() {
+        return retryableInput(RacingGameView::getTryNumsInput, RacingGameView::validateTryNums);
+    }
+
+    private static boolean validateTryNums(int tryNums) {
+        return tryNums > 0;
+    }
+
+    private static int getTryNumsInput() {
+        String input = getInput(INPUT_TRY_NUMS_MSG);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자를 입력해주세요.");
+            return -1;
+        }
+    }
+
+    private static String getInput(String msg) {
+        System.out.println(msg);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public static void displayResult() {
-        System.out.println(Message.RESULT);
+        System.out.println();
+        System.out.println(RESULT_MSG);
     }
 
     public static void displayCars(List<Car> cars) {
@@ -36,6 +71,6 @@ public class RacingGameView {
 
     public static void displayWinners(List<Car> cars) {
         String winners = cars.stream().map(Car::getName).collect(Collectors.joining(", "));
-        System.out.printf(Message.DISPLAY_WINNER_FMT, winners);
+        System.out.printf(DISPLAY_WINNER_FMT, winners);
     }
 }
